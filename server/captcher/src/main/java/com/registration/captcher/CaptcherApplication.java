c4d5a36408afef33ea52c160826dbb3a47159cc0
softcafecommon <softcafe.common@gmail.com>
2023-12-06 14:23:28 +0600

    ok

---
+++ server/captcher/src/main/java/com/registration/captcher/CaptcherApplication.java
@@ -0,0 +1,13 @@
+package com.registration.captcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptcherApplication.class, args);
+	}
+
+}
