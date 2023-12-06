5f7c52abc33936e423b75dba920782866ec3e4de
softcafecommon <softcafe.common@gmail.com>
2023-12-06 15:52:01 +0600

    ok

--- server/captcher/src/main/java/com/registration/captcher/repo/UserRepo.java
+++ server/captcher/src/main/java/com/registration/captcher/repo/UserRepo.java
@@ -1,5 +1,9 @@
 package com.registration.captcher.repo;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.registration.captcher.entiry.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
